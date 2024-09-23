c2ec2142a26883cfbb643baeaeab117bff1f9893
isah274 <81623068+isah274@users.noreply.github.com>
2024-09-23 10:40:05 -0300

    23/09/2024

---
+++ isabellaNutricao-main/main.js
@@ -0,0 +1,13 @@
+alert("oi Isah");
+console.log("alguma coisa");
+
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Isah Nutrição";
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
