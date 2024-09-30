c698ffa6a8308d4314d7a65d7e67f46b57dc8141
isah274 <81623068+isah274@users.noreply.github.com>
2024-09-30 10:41:38 -0300

    30/09/24

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 alert("oi Isah");
 console.log("alguma coisa");
 
-var titulo = document.querySelector("h1");
+var titulo = document.querySelector(".titulo");
 titulo.textContent = "Isah Nutrição";
 
-var trPaulo = document.querySelector("#paulo");
+var paciente = document.querySelector("#primeiro-paciente");
 
-console.log(trPaulo);
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
 
-var tdPeso = trPaulo.querySelector(".info-peso");
-tdPeso.textContent = "150";
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
